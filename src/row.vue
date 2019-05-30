3d72c68d3035cfc3f224dfb75d118b72566d03cb
mich <843298603@qq.com>
2019-05-30 22:57:42 +0800

    重构 row 和 col

--- src/row.vue
+++ src/row.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div class="row" :style="{marginLeft:-gutter / 2 + 'px',marginRight:-gutter / 2 + 'px'}">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
@@ -12,24 +12,22 @@
                 type: [Number, String]
             }
         },
-        created() {
-            console.log(this.$children)
-            //没有儿子
+        computed: {
+            rowStyle() {
+                let {gutter} = this
+                return {
+                    marginLeft: -gutter / 2 + 'px',
+                    marginRight: -gutter / 2 + 'px'
+                }
+            }
         },
         mounted() {
-            console.log(this.$children)
-            console.log(this.$children)
             this.$children.forEach((vm) => {
                 vm.gutter = this.gutter
             })
-            //有儿子
         }
     }
 
-    // var div = document.createElement('div') //created
-    // var childDiv = document.createElement('div') //child created
-    // div.appendChild(childDiv) // child mounted
-    // document.body.appendChild(div) // mounted
 
 </script>
 <style scoped lang="scss">
