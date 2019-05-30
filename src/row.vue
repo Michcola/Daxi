3dcac1ef77b98c8df593469dabd909b5f5700f44
mich <843298603@qq.com>
2019-05-30 22:35:57 +0800

    基本实现row和col的功能

--- src/row.vue
+++ src/row.vue
@@ -1,9 +1,37 @@
 
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter / 2 + 'px',marginRight:-gutter / 2 + 'px'}">
         <slot></slot>
     </div>
 </template>
+<script>
+    export default {
+        name: 'DaxiRow',
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        created() {
+            console.log(this.$children)
+            //没有儿子
+        },
+        mounted() {
+            console.log(this.$children)
+            console.log(this.$children)
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+            //有儿子
+        }
+    }
+
+    // var div = document.createElement('div') //created
+    // var childDiv = document.createElement('div') //child created
+    // div.appendChild(childDiv) // child mounted
+    // document.body.appendChild(div) // mounted
+
+</script>
 <style scoped lang="scss">
     .row {
         display: flex;
