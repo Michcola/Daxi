91f43c95bd4c2a5eafe10203b45d0ae223df9c14
mich <843298603@qq.com>
2019-06-07 12:44:39 +0800

    添加 toast 和 plugin

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ import Content from "./content";
 import Footer from "./footer";
 import Layout from "./layout";
 import Sider from "./sider";
-
+import Toast from "./toast"
+import plugin from './plugin'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon', Icon)
@@ -23,6 +24,8 @@ Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
 Vue.component('g-layout', Layout)
 Vue.component('g-sider', Sider)
+Vue.component('g-toast', Toast)
+Vue.use(plugin)
 
 new Vue({
     el:'#app',
@@ -33,11 +36,10 @@ new Vue({
         message: 'hi'
     },
     created() {
-
     },
     methods: {
-        inputChange(e) {
-            console.log(e)
+        showToast() {
+            this.$toast('我是message')
         }
     }
 })
---
+++ src/toast.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DaxiToast"
+    }
+
+</script>
+
+<style scoped lang="scss">
+    $font-size: 14px;
+    $toast-height: 40px;
+    $toast-bg: black;
+    .toast {
+        font-size: $font-size;
+        height: $toast-height;
+        line-height: 1.8;
+        position: fixed;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        display: flex;
+        align-items: center;
+        background: $toast-bg;
+        border-radius: 4px;
+        box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+        color: white;
+        padding: 0 16px;
+    }
+</style>
