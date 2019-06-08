c54e3619add200f9cfcb41541b40184d8e46dda8
mich <843298603@qq.com>
2019-06-08 23:41:25 +0800

    添加 tabs 相关组件,添加基本 props

---
+++ src/tabs-item.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="tabs-item">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DaxiTabsItem",
+        props: {
+            disabled: {}
+        }
+    }
+</script>
+
+<style scoped>
+    .tabs-item {
+
+    }
+</style>
