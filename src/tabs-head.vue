9e9d729451bed792df869429a9aac3e91bc60fef
mich <843298603@qq.com>
2019-06-09 18:39:37 +0800

    完善tabs的样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -25,12 +25,13 @@
 <style scoped lang="scss">
     $tab-height: 40px;
     $color: blue;
+    $border-color: #ddd;
     .tabs-head {
         display: flex;
         height: $tab-height;
         justify-content: flex-start;
         position: relative;
-
+        border-bottom: 1px solid $border-color;
         > .line {
             position: absolute;
             bottom: 0;
@@ -39,6 +40,10 @@
         }
         > .actions-wrapper {
             margin-left: auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0 1em;
         }
     }
 </style>
--- src/tabs-pane.vue
+++ src/tabs-pane.vue
@@ -36,8 +36,8 @@
 
 <style scoped lang="scss">
     .tabs-pane {
+        padding: 1em;
         &.active {
-            background: red;
         }
     }
 </style>
