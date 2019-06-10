c06996fedf22004f867715b75b68ec4783e64681
mich <843298603@qq.com>
2019-06-10 19:57:47 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -4,7 +4,7 @@
              v-if="visible">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block">
             <slot></slot>
         </span>
     </div>
@@ -27,6 +27,12 @@
                 if (this.$refs.popover &&
                     (this.$refs.popover === e.target ||
                         this.$refs.popover.contains(e.target))
+                ) {
+                    return;
+                }
+                if (this.$refs.contentWrapper &&
+                    (this.$refs.contentWrapper === e.target ||
+                        this.$refs.contentWrapper.contains(e.target))
                 ) {
                     return;
                 }
@@ -59,17 +65,43 @@
 </script>
 
 <style scoped lang="scss">
+    $border-color: #333;
+    $border-radius: 4px;
     .popover {
         display: inline-block;
         vertical-align: top;
         position: relative;
-
     }
-
     .content-wrapper {
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+        background: white;
         transform: translateY(-100%);
+        margin-top: -10px;
+        padding: .5em 1em;
+        max-width: 20em;
+        word-break: break-all;
+
+        &::before, &::after {
+            content: '';
+            display: block;
+            border: 10px solid transparent;
+            width: 0;
+            height: 0;
+            position: absolute;
+            left: 10px;
+        }
+
+        &::before {
+            top: 100%;
+            border-top-color: black;
+        }
+
+        &::after {
+            top: calc(100% - 1px);
+            border-top-color: white;
+        }
     }
 </style>
